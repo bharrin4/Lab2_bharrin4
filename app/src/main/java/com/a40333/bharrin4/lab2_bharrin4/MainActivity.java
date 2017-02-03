ed7cbf9d4ffb4065a2897ee8dd39147dd315bb6f
Brittany Harrington <bharrin4@nd.edu>
2017-02-03 16:10:44 -0500

    Initial commit

---
+++ app/src/main/java/com/a40333/bharrin4/lab2_bharrin4/MainActivity.java
@@ -0,0 +1,14 @@
+package com.a40333.bharrin4.lab2_bharrin4;
+
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
