e8fee87927d39699d29d764793c1649751712d75
Brittany Harrington <bharrin4@nd.edu>
2017-02-19 19:35:12 -0500

    Lab 4 code

---
+++ app/src/main/java/com/a40333/bharrin4/lab2_bharrin4/DetailActivity.java
@@ -0,0 +1,89 @@
+package com.a40333.bharrin4.lab2_bharrin4;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+import static java.security.AccessController.getContext;
+
+/**
+ * Created by User on 2/15/2017.
+ */
+
+public class DetailActivity extends Activity {
+
+    @Override
+
+    public void onCreate (Bundle bundle) {
+
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_detail);
+
+        String[] stringInfo = getIntent().getStringArrayExtra("team");
+
+        TextView gameTime = (TextView) findViewById(R.id.gameTime);
+        gameTime.setText(stringInfo[0]);
+
+        TextView gameLocation = (TextView) findViewById(R.id.gameLocation);
+        gameLocation.setText(stringInfo[1]);
+
+        ImageView opposingLogo = (ImageView) findViewById(R.id.opposingLogo);
+        String mDrawableName = stringInfo[2];
+        int resID = getResources().getIdentifier(mDrawableName , "drawable",  getPackageName());
+        opposingLogo.setImageResource(resID);
+
+        TextView opposingName = (TextView) findViewById(R.id.opposingName);
+        opposingName.setText(stringInfo[3]);
+
+        TextView opposingMascot = (TextView) findViewById(R.id.opposingMascot);
+        opposingMascot.setText(stringInfo[4]);
+
+        TextView opposingRec = (TextView) findViewById(R.id.opposingRec);
+        opposingRec.setText(stringInfo[5]);
+
+        TextView scoreID = (TextView) findViewById(R.id.scoreID);
+        scoreID.setText(stringInfo[6]);
+
+        TextView finalString = (TextView) findViewById(R.id.finalString);
+        finalString.setText(stringInfo[7]);
+
+        TextView ndName = (TextView) findViewById(R.id.ndName);
+        ndName.setText(stringInfo[8]);
+
+        TextView ndMascot = (TextView) findViewById(R.id.ndMascot);
+        ndMascot.setText(stringInfo[9]);
+
+        TextView ndRec = (TextView) findViewById(R.id.ndRec);
+        ndRec.setText(stringInfo[10]);
+
+        ImageView ndLogo = (ImageView) findViewById(R.id.ndLogo);
+        String mDrawableName2 = stringInfo[11];
+        int resID2 = getResources().getIdentifier(mDrawableName2 , "drawable",  getPackageName());
+        ndLogo.setImageResource(resID2);
+
+        Button cameraButton = (Button) findViewById(R.id.cameraButton);
+        cameraButton.setText(stringInfo[12]);
+        cameraButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(cameraIntent);
+            }
+        });
+    }
+
+}
+
--- app/src/main/java/com/a40333/bharrin4/lab2_bharrin4/MainActivity.java
+++ app/src/main/java/com/a40333/bharrin4/lab2_bharrin4/MainActivity.java
@@ -1,27 +1,48 @@
 package com.a40333.bharrin4.lab2_bharrin4;
 
 //import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class MainActivity extends Activity {
-
-
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList al = new ArrayList<String []>();
-        //add teams to array
+       //specific game info
+        final ArrayList <String []> arr = new ArrayList<String []>();
+        arr.add( new String []{"Saturday, February 11, 6:00pm", "Purcell Pavilion at the Joyce Center, Notre Dame, Indiana", "fsu_sports_logo",
+        "Florida State", "Seminoles", "(21-5)", "72-84", "Final", "Notre Dame", "Fighting Irish", "(21-7)", "nd_logo", "Camera"});
+        arr.add( new String []{"Tuesday, February 14, 7:00pm", "Silvio O. Conte Forum, Chestnut Hill, Massachusetts", "bc_logo",
+                "Boston College", "Eagles", "(9-18)", "76-84", "Final", "Notre Dame", "Fighting Irish", "(21-7)", "nd_logo", "Camera"});
+        arr.add( new String []{"Saturday, February 18, 12:00pm", "PNC Area, Raleigh, North Carolina", "nc_state",
+                "North Carolina State", "Wolfpack", "(14-14)", "72-81", "Final", "Notre Dame", "Fighting Irish", "(21-7)", "nd_logo", "Camera"});
+        arr.add( new String []{"Sunday, February 26, 6:30pm", "Purcell Pavilion at the Joyce Center, Notre Dame, Indiana", "gtech_logo",
+                "Georgia Tech", "Yellow Jackets", "(15-11)", "@", "Final", "Notre Dame", "Fighting Irish", "(21-7)", "nd_logo", "Camera"});
+        arr.add( new String []{"Wednesday, March 1, 8:00pm", "Purcell Pavilion at the Joyce Center, Notre Dame, Indiana", "bc_logo",
+                "Boston College", "Eagles", "(9-18)", "@", "Final", "Notre Dame", "Fighting Irish", "(21-7)", "nd_logo", "Camera"});
+        arr.add( new String []{"Saturday, March 4, 2:00pm", "KFC Yum! Center, Louisville, Kentucky", "louisville",
+                "Louisville", "Cardinals", "(22-5)", "@", "Final", "Notre Dame", "Fighting Irish", "(21-7)", "nd_logo", "Camera"});
+        arr.add( new String []{"Tuesday, March 7, 12:00pm", "Barclay's Center, New York, New York", "acc",
+                "ACC Tournament", "N/A", "N/A", "@", "Final", "Notre Dame", "Fighting Irish", "(21-7)", "nd_logo", "Camera"});
+        arr.add( new String []{"Thursday, March 16", "N/A", "ncaa",
+                "NCAA Tournament", "N/A", "N/A", "@", "Final", "Notre Dame", "Fighting Irish", "(21-7)", "nd_logo", "Camera"});
 
+        final ArrayList <String []> al = new ArrayList <String []>();
+        //add teams to array
         al.add(new String[]{"fsu_sports_logo", "Florida State", "Feb 11"});
         al.add(new String[]{"bc_logo", "Boston College", "Feb 14"});
         al.add(new String[]{"nc_state", "North Carolina State", "Feb 18"});
@@ -32,9 +53,19 @@ public class MainActivity extends Activity {
         al.add(new String[]{"ncaa", "NCAA Tournament", "March 16"});
 
 
-        ScheduleAdapter scheduleAdapter = new ScheduleAdapter(this, al);
+        ScheduleAdapter scheduleAdapter = new ScheduleAdapter(getApplicationContext(), al);
         ListView scheduleListView = (ListView) findViewById(R.id.scheduleListView);
         scheduleListView.setAdapter(scheduleAdapter);
 
+        AdapterView.OnItemClickListener clickListener = new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent mIntent = new Intent(MainActivity.this, DetailActivity.class);
+                mIntent.putExtra("team", arr.get(position));
+                startActivity(mIntent);
+            }
+        };
+        // this will automatically attach the listener to each item of the listview.
+        scheduleListView.setOnItemClickListener (clickListener);
     }
 }
