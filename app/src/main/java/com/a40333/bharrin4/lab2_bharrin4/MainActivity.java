124544a18556315332e55e95437ba401bf0ee43e
Brittany Harrington <bharrin4@nd.edu>
2017-03-21 15:07:33 -0400

    Finished lab 6 files

--- app/src/main/java/com/a40333/bharrin4/lab2_bharrin4/MainActivity.java
+++ app/src/main/java/com/a40333/bharrin4/lab2_bharrin4/MainActivity.java
@@ -1,32 +1,26 @@
 package com.a40333.bharrin4.lab2_bharrin4;
 
-//import android.support.v7.app.AppCompatActivity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    @Override
+    ArrayList<Team> al = new ArrayList<Team>();
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -38,8 +32,7 @@ public class MainActivity extends AppCompatActivity {
         MyCsvFileReader myCsvFileReader = new MyCsvFileReader(getApplicationContext());
         String mDrawableName = "schedule";
         int resID = getResources().getIdentifier(mDrawableName , "raw", getPackageName());
-        Log.d("tag", "resID="+resID);
-        final ArrayList <Team> al = myCsvFileReader.readCsvFile(resID);
+        al = myCsvFileReader.readCsvFile(resID);
 
 
 
@@ -66,29 +59,95 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    public String gameSchedule() {
+        StringBuilder str = new StringBuilder();
+
+        for (Team s : al)
+        {
+            str.append(s.getOpposingName());
+            str.append("\t");
+            str.append(s.getGameLocation());
+            str.append("\t");
+            str.append(s.getDate());
+            str.append("\n");
+        }
+
+        String string = str.toString();
+        Log.d("string", string);
+        return string;
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int res_id = item.getItemId();
 
         if (res_id == R.id.share) {
-            /*code for sharing the schedule
+            //code for sharing the schedule
             Intent shareIntent = new Intent();
             shareIntent.setAction(android.content.Intent.ACTION_SEND);
-            shareIntent.setType("text/plain");
-            shareIntent.putExtra("android.content.Intent.EXTRA_SUBJECT", "BasketBall Matches");
-            shareIntent.putExtra("android.content.Intent.EXTRA_TEXT", gameSchedule());
-            startActivity(Intent.createChooser(shareIntent), "Share via"); */
+            shareIntent.setType("plain/text");
+            shareIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "BasketBall Matches");
+            shareIntent.putExtra(android.content.Intent.EXTRA_TEXT, gameSchedule());
+            startActivity(android.content.Intent.createChooser(shareIntent, "Share via"));
         }
 
         else if (res_id == R.id.sync) {
             // Snackbar with Try Again action
+            final CoordinatorLayout coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorlayout);
+
+            Snackbar snackbar = Snackbar.make(coordinatorLayout, "Sync is not yet implemented", Snackbar.LENGTH_LONG);
+
+            snackbar.setAction("Try Again", new View.OnClickListener() {
+
+                @Override
+
+                public void onClick(View v) {
+
+                    Snackbar.make(coordinatorLayout, "Wait for the next few labs. Thank you for your patience", Snackbar.LENGTH_LONG).show();
+
+                }
+
+            });
+
+            snackbar.show();
+
         }
 
         else if (res_id == R.id.settings) {
             // Floating Contextual Menu with options
+            View view = (View) findViewById(R.id.scheduleListView);
+            registerForContextMenu(view);
+            openContextMenu(view);
+            unregisterForContextMenu(view);
 
         }
         return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        //code to inflate floating_contextual_menu
+        getMenuInflater().inflate(R.menu.floating_contextual_menu, menu);
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        int item_id = item.getItemId();
+        if (item_id == R.id.women) {
+            //to be implemented later
+        }
+        if (item_id == R.id.men) {
+            //to be implemented later
+        }
+        if (item_id == R.id.on_campus) {
+            //to be implemented later
+        }
+        if (item_id == R.id.off_campus) {
+            //to be implemented later
+        }
+        return false;
     }
 
 }
